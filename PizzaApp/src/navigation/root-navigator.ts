90319a0d40acd67f7459506dc8c4026d0a92a2d8
thinhvoxuan <thinhvoxuan@gmail.com>
2018-05-20 14:30:20 +0700

    add vector icon

--- PizzaApp/src/navigation/root-navigator.ts
+++ PizzaApp/src/navigation/root-navigator.ts
@@ -1,9 +1,11 @@
 import { StackNavigator } from "react-navigation"
+import { PizzaLocationList } from "../views/pizza-location-list/pizza-location-list-screen"
 // import { PizzaLocationList } from "../views/example/pizza-location-list/pizza-location-list-screen"
 import { ExampleNavigator } from "./example-navigator"
 
 export const RootNavigator = StackNavigator(
   {
+    PizzaLocationListNavigator: { screen: PizzaLocationList },
     exampleStack: { screen: ExampleNavigator },
   },
   {
