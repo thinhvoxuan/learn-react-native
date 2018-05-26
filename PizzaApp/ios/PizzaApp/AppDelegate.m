32579af1375fe3129d82ac3856d142f4ef1ed4ab
thinhvoxuan <thinhvoxuan@gmail.com>
2018-05-26 16:31:04 +0700

    add chat function into list view

--- PizzaApp/ios/PizzaApp/AppDelegate.m
+++ PizzaApp/ios/PizzaApp/AppDelegate.m
@@ -9,6 +9,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <ZDCChat/ZDCChat.h>
+
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
 
 @implementation AppDelegate
 
@@ -18,15 +23,31 @@
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"PizzaApp"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
+  RCTBridge *bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation
+                                            moduleProvider:nil
+                                             launchOptions:launchOptions];
+#if RCT_DEV
+  [bridge moduleForClass:[RCTDevLoadingView class]];
+#endif
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"PizzaApp"
+                                            initialProperties:nil];
+
+  // RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+  //                                                     moduleName:@"PizzaApp"
+  //                                              initialProperties:nil
+  //                                                  launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
+
+  [ZDCChat initializeWithAccountKey:@"x"];
+  [[[ZDCChat instance] overlay] setEnabled:NO];
+  [[ZDCAgentAttachmentCell appearance] setActivityIndicatorViewStyle:@(UIActivityIndicatorViewStyleGray)];
+  [[ZDCVisitorAttachmentCell appearance] setActivityIndicatorViewStyle:@(UIActivityIndicatorViewStyleGray)];
+
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
--- PizzaApp/src/views/pizza/pizza-location-list/pizza-location-list-screen.tsx
+++ PizzaApp/src/views/pizza/pizza-location-list/pizza-location-list-screen.tsx
@@ -3,11 +3,21 @@ import { observer } from "mobx-react"
 import { Text } from "../../shared/text"
 import { NavigationScreenProps } from "react-navigation"
 import { Screen } from "../../shared/screen"
-import { View, Image, ViewStyle, TextStyle, ImageStyle, SafeAreaView, StatusBar } from "react-native"
+import {
+  View,
+  Image,
+  ViewStyle,
+  TextStyle,
+  ImageStyle,
+  SafeAreaView,
+  StatusBar,
+} from "react-native"
 import { color, spacing } from "../../../theme"
 import { Button } from "../../shared/button"
 
-const TEXT: TextStyle = { 
+import ZendeskChat from 'react-native-zendesk-chat';
+
+const TEXT: TextStyle = {
   color: color.palette.white,
   fontFamily: "Montserrat",
 }
@@ -15,11 +25,11 @@ const BOLD: TextStyle = { fontWeight: "bold" }
 
 const FOOTER: ViewStyle = { backgroundColor: "#20162D" }
 const FOOTER_CONTENT: ViewStyle = {
-  paddingVertical: spacing[4], 
+  paddingVertical: spacing[4],
   paddingHorizontal: spacing[4],
 }
-const CONTINUE: ViewStyle = { 
-  paddingVertical: spacing[4], 
+const CONTINUE: ViewStyle = {
+  paddingVertical: spacing[4],
   paddingHorizontal: spacing[4],
   backgroundColor: "#5D2555",
 }
@@ -30,28 +40,20 @@ const CONTINUE_TEXT: TextStyle = {
   letterSpacing: 2,
 }
 
-export interface PizzaLocationListScreenProps extends NavigationScreenProps<{}> {
-}
+export interface PizzaLocationListScreenProps extends NavigationScreenProps<{}> {}
 
 // @inject("mobxstuff")
 @observer
 export class PizzaLocationList extends React.Component<PizzaLocationListScreenProps, {}> {
-  nextScreen = () => this.props.navigation.navigate("detailScreen")
-  render () {
+  nextScreen = () => {
+    ZendeskChat.startChat({})
+  }
+  render() {
     console.tron.log("go here")
     return (
-      <Screen preset="fixedCenter">
-        <Text preset="header" tx="PizzaLocationList.header" />
-        <SafeAreaView style={FOOTER}>
-          <View style={FOOTER_CONTENT}>
-            <Button
-              style={CONTINUE}
-              textStyle={CONTINUE_TEXT}
-              tx="firstExampleScreen.continue"
-              onPress={this.nextScreen}
-              />
-          </View>
-        </SafeAreaView>
+      <Screen>
+        <Text preset="header" tx="PizzaDetailScreen.header" />
+        <Button tx="firstExampleScreen.goBack" onPress={this.nextScreen} />
       </Screen>
     )
   }
